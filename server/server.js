065a04f464605a503cdeb8466c6f219cbefc203f
Bo Kim <bohyung0110@gmail.com>
2022-03-24 15:02:40 -0700

    version 0.6

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "../website")));
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../website/index.html"));
-});
+app.use(express.static(path.join(__dirname, "../website")));
 
 const fortune = ["A pleasant surprise is waiting for you.",
   "A smooth long journey! Great expectations.",
